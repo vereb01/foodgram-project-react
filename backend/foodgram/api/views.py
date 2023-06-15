f734b7368ed97d2d5414d683acacecddfc35d984
Анастасия Гугас <vereb01@mail.ru>
2023-06-15 15:49:40 +0300

    search

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -104,7 +104,7 @@ class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
     permission_classes = (IsAuthenticatedOrReadOnly,)
     pagination_class = None
     filter_backends = [filters.SearchFilter]
-    search_fields = ['^name',]
+    search_fields = ['^name', ]
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
